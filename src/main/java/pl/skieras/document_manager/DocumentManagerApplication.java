caf5eaae52cb4715774084ab7c1729efd3bca7f3
skieras <lskierkowski@bluesoft.com>
2025-03-25 17:28:23 +0100

    Intializer code

---
+++ src/main/java/pl/skieras/document_manager/DocumentManagerApplication.java
@@ -0,0 +1,13 @@
+package pl.skieras.document_manager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DocumentManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DocumentManagerApplication.class, args);
+	}
+
+}
